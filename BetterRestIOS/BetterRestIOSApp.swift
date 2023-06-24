7de39bc20052a387274817f0c8365582f7411c66
vpasamic <vpasamic22@gmail.com>
2023-06-24 15:38:36 -0700

    Initial Commit

---
+++ BetterRestIOS/BetterRestIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestIOSApp.swift
+//  BetterRestIOS
+//
+//  Created by Mission Bit on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRestIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRestIOS
+//
+//  Created by Mission Bit on 6/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
